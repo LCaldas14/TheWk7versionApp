74849569df838f351cae8fba9fb35a34e5337fdb
LCaldas14 <larais@gmail.com>
2022-12-11 20:05:10 +0000

    Updated Time

--- src/app.js
+++ src/app.js
@@ -1,22 +1,39 @@
+function formatTime(timestamp) {
+  let time = new Date(timestamp);
+  let hours = time.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = time.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let countryElement = document.querySelector("#country");
-  countryElement.innerHTML = response.data.country;
-  cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-
   let descriptionElement = document.querySelector("#description");
+  let feelsLikeElement = document.querySelector("#feelsLike");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#windSpeed");
+  let timeElement = document.querySelector("#time");
+
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  countryElement.innerHTML = response.data.country;
   descriptionElement.innerHTML = response.data.condition.description;
 
-  let feelsLikeElement = document.querySelector("#feelsLike");
   feelsLikeElement.innerHTML = Math.round(response.data.temperature.feels_like);
 
-  let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
-  let windSpeedElement = document.querySelector("#windSpeed");
+
   windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+
+  timeElement.innerHTML = formatTime(response.data.time * 1000);
 }
 
 let apiKey = "333f557et08fd3a7702f775fo4452b95";
