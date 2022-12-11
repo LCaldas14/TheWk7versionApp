f02ff75541ae673f990a990a24ed0836888059d0
LCaldas14 <larais@gmail.com>
2022-12-11 15:53:29 +0000

    Showing Current Temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let countryElement = document.querySelector("#country");
+  countryElement.innerHTML = response.data.country;
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+
+  let feelsLikeElement = document.querySelector("#feelsLike");
+  feelsLikeElement.innerHTML = Math.round(response.data.temperature.feels_like);
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
+  let windSpeedElement = document.querySelector("#windSpeed");
+  windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "333f557et08fd3a7702f775fo4452b95";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=London&key=333f557et08fd3a7702f775fo4452b95&units=metric";
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
